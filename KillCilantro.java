c297ac778dbe88b7295dfaebcc8d26d8771591ec
MelaniaRyazanov <melania.ryazanov@gmail.com>
2025-12-12 09:33:34 -0800

    if statement and for loop

--- KillCilantro.java
+++ KillCilantro.java
@@ -1,8 +1,21 @@
 public class KillCilantro{
-   public static String killCilantro(String parameter){
-      
-   }
    public static void main(String[]args){
+      
+     String killCilantro = killCilantro("cilantro");
    
    }
+   public static String killCilantro(String parameter){
+      
+      if(parameter == "cilantro" ){
+       for(int i = 1;  i <= 1; i++){
+            parameter = "parsley";
+            System.out.println(parameter);
+         }
+      }
+   
+      
+       return parameter; ///fix
+   }
+  
+
 }

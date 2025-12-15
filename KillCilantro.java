8d9ebe3fd68f76ed4e6e86dd649454c778406a55
MelaniaRyazanov <melania.ryazanov@gmail.com>
2025-12-15 09:10:51 -0800

    for loop, indexOf, substring, and print

--- KillCilantro.java
+++ KillCilantro.java
@@ -1,20 +1,26 @@
 public class KillCilantro{
    public static void main(String[]args){
       
-     String killCilantro = killCilantro("cilantro");
+     //String killCilantro = 
+          System.out.println(killCilantro("something cilantro something"));
+          System.out.println(killCilantro("cilantro something"));
+          
+
+     
    
    }
-   public static String killCilantro(String parameter){
-      
-      if(parameter == "cilantro" ){
-       for(int i = 1;  i <= 1; i++){
-            parameter = "parsley";
-            System.out.println(parameter);
+   public static String killCilantro(String inputStr){
+      String s = null;
+       for(s = inputStr; s.indexOf("cilantro") != -1;){
+            int index = s.indexOf("cilantro");
+            String s1 = s.substring(0, index);
+            String s2 = s.substring(index + "cilantro".length(), inputStr.length());
+            s = s1 + "parsley" + s2;
+            
          }
-      }
-   
+     
       
-       return parameter; ///fix
+       return s; ///fix
    }
   
 
